d30fb996d42606793dc7985a6d661992f63939e1
Melek Nur <meleknurbacakli@gmail.com>
2025-10-04 16:35:17 +0300

    Update project URLs and set home view as root path

--- chess_coach/urls.py
+++ chess_coach/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'), name='users'),
-    path('home/',user_views.home, name='home'),
+    path('users/', include('users.urls')),
+    path('',views.home, name='home'),
 ]
