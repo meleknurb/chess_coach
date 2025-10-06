bf9a9747490af2a1320a17daebe21d56073b1238
Melek Nur <meleknurbacakli@gmail.com>
2025-10-06 15:55:14 +0300

    Add ChessGameAdmin to admin.py for improved admin interface
    - Registered ChessGame model with custom admin
    - Configured list_display, list_filter, search_fields, and date_hierarchy
    - Added fieldsets for organizing game details and player info

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import ChessGame
+
+class ChessGameAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game_date', 'time_control', 'white_player', 'black_player', 'result_description', 'moves_count', 'cached_at')
+    list_filter = ('game_date', 'time_control', 'result_description', 'user')
+    
+    search_fields = ('white_player', 'black_player', 'result_description', 'user__username')
+    date_hierarchy = 'game_date'
+    
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'pgn'),
+        }),
+        ('Game Details', {
+            'fields': ('game_date', 'time_control', 'result_description', 'moves_count'),
+            'classes': ('collapse',),
+        }),
+        ('Players', {
+            'fields': ('white_player', 'black_player'),
+        }),
+    )
+
+admin.site.register(ChessGame, ChessGameAdmin)
